e5470eecb99344feba6dd5755772ef140d025460
Krzysztof333 <kbutlewski@gmail.com>
2019-01-10 12:00:40 +0100

    Zatwierdzono 2

--- test1.cs
+++ test1.cs
@@ -11,6 +11,7 @@ public class testowy_script1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.log("Hello World!");
+        Debug.log("dalsze zmiany");
 	}
 	
 	// Update is called once per frame
