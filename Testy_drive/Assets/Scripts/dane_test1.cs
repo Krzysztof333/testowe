05c075be213caa6ea2a7e354cd129c2c34a3978d
Krzysztof333 <kbutlewski@gmail.com>
2019-01-16 13:12:42 +0100

    wprowadzono nowy wiersz

--- Testy_drive/Assets/Scripts/dane_test1.cs
+++ Testy_drive/Assets/Scripts/dane_test1.cs
@@ -7,7 +7,7 @@ public class dane_test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Obiekt zoastał zapisany"); 
     }
 
     // Update is called once per frame
