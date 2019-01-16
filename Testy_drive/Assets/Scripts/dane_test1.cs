41573b8011e37273910a6ad218f57e60db9144d3
Krzysztof333 <kbutlewski@gmail.com>
2019-01-16 13:19:35 +0100

    wprowadzono33

--- Testy_drive/Assets/Scripts/dane_test1.cs
+++ Testy_drive/Assets/Scripts/dane_test1.cs
@@ -7,7 +7,10 @@ public class dane_test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Obiekt zoastał zapisany"); 
+        Debug.Log("Obiekt zoastał zapisany");
+        Debug.log("zmiany początkowe");
+        Debug.log("dalsze zmiany");
+        Debug.log("Każdą zmianę trzeba zatwierdzić przed wypchnięciem");
     }
 
     // Update is called once per frame
